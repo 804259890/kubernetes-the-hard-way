f42a1f94b0592e787f4c9bedca406d22b061954c
Trevor <passeam@gmail.com>
2021-02-20 12:49:31 +0800

    update package sources

--- vagrant/ubuntu/install-docker-2.sh
+++ vagrant/ubuntu/install-docker-2.sh
@@ -1,3 +1,3 @@
 cd /tmp
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh /tmp/get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh 
+cat /tmp/get-docker.sh | bash -s docker --mirror Aliyun
